ff661f638c4fcc6135d808484e19975f49f6d972
Rayan Chikhi <chikhi@psu.edu>
2013-08-11 15:51:06 -0400

    initial import of gatb-benchmark, for kmer counting

---
+++ kmer_counting/benchmark.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+"""
+Usage: benchmark <reads_file> <temp_dir>
+"""
+import sys, os, subprocess, fileinput
+from datetime import datetime 
+import yaml
+from codespeed_client import save_to_speedcenter
+
+if len(sys.argv) < 1:
+    sys.exit("arguments: file1.yaml file2.yaml ...")
+
+config = '\n'.join([l for l in fileinput.input(sys.argv[1:])])
+b = yaml.load(config)
+
+def run(command):
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    rc = process.wait()
+    return rc, stdout, stderr
+
+environment = b["environment"]
+
+def analyze_log(stdout, stderr):
+    memory = 0
+    time = 0
+    for line in stdout.split('\n'):
+        if line.startswith("maximal memory used"):
+            memory = "%0.2f" % (float(line.split()[-1])/1024.0/1024.0)
+            print memory,line
+    for line in stderr.split('\n'):
+        if line.startswith("real"):
+            t = datetime.strptime(line.split()[-1],"%Mm%S.%fs")
+            time = "%0.2f" % (t.minute + t.second/60.0)
+    return time, memory
+
+for prog_item in b["program"]:
+    description = prog_item["description"] 
+    cmd = prog_item["command line"] % b
+    executable = prog_item["executable"]
+    project = prog_item["project"]
+
+    print "Runnning",description
+    dsk = "DSK" in project
+    svn = False
+    git = False
+    if "DSK SVN" in description:
+        svn = True
+        svn_folder = "~/dsk"
+
+    if "DSK GATB" in description:
+        git = True
+        git_folder = "~/gatb-tools/gatb-tools/"
+
+    if svn:
+        revision = run("svn info %s | sed -ne 's/^Revision: //p'" % svn_folder)[1].strip()
+
+    if git:
+        revision = run("cd ~/gatb-tools/ &&  git log -n 1 --pretty='format:%h (%ci)'")[1].strip()
+
+    if "KMC" in description:
+        revision = "0.3"
+
+    rc, stdout, stderr = run(cmd)
+    if rc:
+        sys.exit("Error running command: %s" % cmd)
+    
+    time, memory = analyze_log(stdout,stderr)
+
+    print "Sending benchmark data"
+    url = "http://codespeed.genouest.org/result/add/"
+    branch = "default"
+
+    benchmark = "Time"
+    result_value = time
+    save_to_speedcenter(url, project, revision, executable, benchmark, result_value, branch=branch, environment=environment)
+
+    benchmark = "Memory"
+    result_value = memory
+    save_to_speedcenter(url, project, revision, executable, benchmark, result_value, branch=branch, environment=environment)
